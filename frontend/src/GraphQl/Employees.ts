5cfbab08dc8dd0924baf3dcf2b55f6bccf8f0c77
Anil Kumar Putta <114547466+Anilkumarputta@users.noreply.github.com>
2025-11-30 16:40:11 -0600

    Add GraphQL queries and mutations for employees

--- frontend/src/GraphQl/Employees.ts
+++ frontend/src/GraphQl/Employees.ts
@@ -1,1 +1,45 @@
+import { gql } from "graphql-request";
 
+export const EMPLOYEES_QUERY = gql`
+  query Employees($page: Int, $perPage: Int, $filter: EmployeeFilter) {
+    employees(page: $page, perPage: $perPage, filter: $filter) {
+      items {
+        id
+        fullName
+        className
+        attendancePercentage
+      }
+      total
+      page
+      perPage
+    }
+  }
+`;
+
+export const CREATE_EMPLOYEE = gql`
+  mutation CreateEmployee($input: EmployeeInput!) {
+    createEmployee(input: $input) {
+      id
+      fullName
+      className
+      attendancePercentage
+    }
+  }
+`;
+
+export const UPDATE_EMPLOYEE = gql`
+  mutation UpdateEmployee($id: Int!, $input: EmployeeInput!) {
+    updateEmployee(id: $id, input: $input) {
+      id
+      fullName
+      className
+      attendancePercentage
+    }
+  }
+`;
+
+export const DELETE_EMPLOYEE = gql`
+  mutation DeleteEmployee($id: Int!) {
+    deleteEmployee(id: $id)
+  }
+`;
